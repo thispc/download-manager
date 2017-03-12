ff74e94c1b4fc0ff58bd29a82405d2c8adb14cd0
thispc <pulkitchawl@gmail.com>
2017-03-13 03:41:10 +0530

    Major Changes

---
+++ main.py
@@ -0,0 +1,61 @@
+import json
+from pprint import pprint
+import os
+import wget
+import optparse
+import time
+import signal
+
+def signal_handler(signal, frame):
+    print 'You pressed Ctrl+C!'
+    sys.exit(0)
+def download(ssid):
+	try:
+		#print "debug1"
+		with open('users.dat') as data_file:    
+			data = json.load(data_file)
+    		#print "debug2"
+    		l = data[ssid-1]
+    		#print "debug3"
+    		print "\n"+str(len(l["url"]))+" downloads to be performed. Download ??y/n\n"
+    		inp=str(raw_input())
+    		if(inp[0]=='n' or inp[0]=='N'):
+    			raise ValueError("Exiting...")
+    			sys.exit(2)
+    		#signal.signal(signal.SIGINT, signal_handler)
+    		for j in l["url"]:
+    			os.system("wget "+j)
+    		print "Downloaded Successfully. Inform %s" % l["user"]
+    	except (IOError, ValueError, KeyError, IndexError, TypeError) as error:
+    		raise error
+    		print "File is invalid or ssid is incorrect"
+
+
+def showrequest():
+	with open('users.dat') as data_file:
+		data = json.load(data_file)
+		ss=1
+    	for i in data:
+    		print "============================================================================="
+    		print "id : %s" % str(ss)
+    		print "user : %s" % i["user"]
+    		print "url : %s" % i["url"]
+    		print "volunteered : %s" % i["volun"]
+    		print "Done? : %s" % i["done"]
+    		ss=ss+1
+
+    	print "============================================================================="
+if __name__ == "__main__":
+
+	os.system("python scraper.py")
+	parser = optparse.OptionParser('usage: %prog [options]')
+	parser.add_option("--show", "-s", dest="sflag",action="store_true", help="Show Request")
+	parser.add_option("--select", "-i", dest="ssid",action="store",type="int", help="Index of link")
+	#parser.add_option("--link", "-l", dest="ulink",action="store", help="New Database For Search")
+	#parser.add_option("--name", "-n", dest="uname",action="store", help="Name of Database")
+	(options, args) = parser.parse_args()
+	if(options.sflag):
+		showrequest()
+	if(options.ssid is not None):
+		download(options.ssid)
+
--- scraper.py
+++ scraper.py
@@ -14,29 +14,39 @@ from lxml import etree
 nn="""\b(https?|ftp|file):\/\/[-A-Z0-9+&@#(\/%?=~_|!:,.;]*[-A-Z0-9+&@#)\/%=~_|]"""
 M=re.compile(nn)
 url="http://172.16.86.222/dchub/request&page="
-
+userlinks =[]
 for i in range(1,6):
 	tempurl=url+str(i)
 	html_page = urllib2.urlopen(tempurl)
 	soup = BeautifulSoup(html_page,"lxml")
-	print "============="
+	sys.stdout.write(".")
 	for j in range(1,11):
 		link=soup.select('body > div.container > div.row > div.span7 > div:nth-of-type('+str(j)+') > div.post')
 		user=soup.select('body > div.container > div.row > div.span7 > div:nth-of-type('+str(j)+') > h4 > a')
-		#urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(link))
-		urls=M.findall(str(link))
-		print urls
-		for k in urls:
-			pos=0
-			for f in k:
-				pos=pos+1
-				if f=='\\':
-					break
-			#print k[:pos-1]
-		if(len(user)!=0):	
-			print user[0].text
+		if(len(link)):
+			urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(link[0].text))
 		else:
-			print "Nil"
+			continue
+		#urls=M.findall(str(link))
+		if(len(user)!=0):	
+			#print user[0].text
+			temp=dict()
+			temp["user"]=user[0].text
+			temp["url"]=[]
+			temp["done"]="No"
+			temp["volun"]="false"
+			for k in urls:
+				temp["url"].append(k)
+			if(len(temp["url"])!=0):
+				userlinks.append(temp)
+		
+		#	for f in k:
+		#		pos=pos+1
+		#		if f=='\\':
+		#			break
+			#print k[:pos-1]
+print "\n"
+json.dump(userlinks, open('users.dat', 'w'))
 
 		#sel=CSSSelector('body div.container div.row div.span7 div:nth-child(10) div.post a')
 		#print sel()
