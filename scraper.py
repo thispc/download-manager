783434919c85d0ff549195fcea55e93a8d3e9f7b
thispc <pulkitchawl@gmail.com>
2017-03-13 01:22:13 +0530

    changes

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import urllib
 import urllib2
 import re
@@ -9,15 +9,34 @@ import optparse
 import lxml.html
 from lxml import etree
 #from lxml.cssselect import CSSSelector
-
+#rr='\b(magnet):?[-A-Z0-9+&@#()\/%?=~_|!:,.;]*[-A-Z0-9+&@#()\/%=~_|]'
+#N= re.compile(rr)
+nn="""\b(https?|ftp|file):\/\/[-A-Z0-9+&@#(\/%?=~_|!:,.;]*[-A-Z0-9+&@#)\/%=~_|]"""
+M=re.compile(nn)
 url="http://172.16.86.222/dchub/request&page="
 
-for i in range(1,2):
+for i in range(1,6):
 	tempurl=url+str(i)
-	r = requests.get(tempurl)
 	html_page = urllib2.urlopen(tempurl)
-	soup = BeautifulSoup(html_page)
-	link=soup.select('body')
-	print link.text
-	#sel=CSSSelector('body div.container div.row div.span7 div:nth-child(10) div.post a')
-	#print sel()
+	soup = BeautifulSoup(html_page,"lxml")
+	print "============="
+	for j in range(1,11):
+		link=soup.select('body > div.container > div.row > div.span7 > div:nth-of-type('+str(j)+') > div.post')
+		user=soup.select('body > div.container > div.row > div.span7 > div:nth-of-type('+str(j)+') > h4 > a')
+		#urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(link))
+		urls=M.findall(str(link))
+		print urls
+		for k in urls:
+			pos=0
+			for f in k:
+				pos=pos+1
+				if f=='\\':
+					break
+			#print k[:pos-1]
+		if(len(user)!=0):	
+			print user[0].text
+		else:
+			print "Nil"
+
+		#sel=CSSSelector('body div.container div.row div.span7 div:nth-child(10) div.post a')
+		#print sel()
