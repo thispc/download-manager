6b3860a2bbe0f807aa675a1165179c8a9244e2c4
Tanmay Gupta <tanmay.tat11@gmail.com>
2017-03-17 16:34:09 +0530

    client.py

---
+++ client.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+username = input('Enter your username: ')
+#test123
+
+password = input('Enter your password: ')
+#123456
+
+secret = "qwerty"
+
+url = "172.16.86.222:13000/login?nick="
+url += username
+url += "&password="
+url += password
+url += "&secret="
+url += secret
+
+r = requests.get(url)
+
+js = json.loads(r.text)
